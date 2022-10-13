b58b5e19875f40d12a29d2c918dfa4b65d72342e
K.R. Zentner <krzentner@gmail.com>
2022-10-13 02:07:40 -0700

    Plotting tweaks

--- src/gp_ucb_algo.py
+++ src/gp_ucb_algo.py
@@ -8,17 +8,15 @@ from dowel import logger, tabular
 from garage.experiment.deterministic import get_seed
 from garage.np.algos import RLAlgorithm
 
-# DEFAULT_GP_KERNEL = (Matern(length_scale=0.01, nu=1.5,
-                            # length_scale_bounds="fixed") +
-                     # (WhiteKernel() *
-                      # Matern(length_scale=0.01, nu=1.5,
-                             # length_scale_bounds="fixed")))
-DEFAULT_GP_KERNEL = (Matern(length_scale=0.05, nu=0.5,
+DEFAULT_GP_KERNEL = (Matern(length_scale=0.01, nu=1.5,
                             length_scale_bounds="fixed") +
                      ConstantKernel() +
-                     (ConstantKernel(0.01) * WhiteKernel() *
-                      Matern(length_scale=0.01, nu=0.5,
+                     (WhiteKernel() *
+                      Matern(length_scale=0.01, nu=1.5,
                              length_scale_bounds="fixed")))
+# DEFAULT_GP_KERNEL = (Matern(length_scale=0.05, nu=1.5,
+                            # length_scale_bounds="fixed") +
+                     # ConstantKernel())
 
 
 class GPUCBAlgo(RLAlgorithm):
@@ -37,7 +35,7 @@ class GPUCBAlgo(RLAlgorithm):
         self.hparam_ranges = self.inner_algo.hparam_ranges()
         self.n_hparams = len(self.hparam_ranges)
         self.n_hparam_ucb_samples = 1000
-        self.ucb_nu = 8
+        self.ucb_nu = 2
         self.ucb_delta = 0.5
 
     def _hparams_to_vec(self, hparams):
@@ -74,7 +72,7 @@ class GPUCBAlgo(RLAlgorithm):
             self.regressor.fit(self.hparam_vecs, self.perf_changes)
             for index, (key, value) in enumerate(self.hparam_ranges.items()):
                 self.plot_gpr(f'{trainer.log_directory}/{key}_{epoch:05}.png',
-                              key, index)
+                              key, index, epoch)
         self.prev_algo_perf = perf
         return new_stats
 
@@ -111,7 +109,7 @@ class GPUCBAlgo(RLAlgorithm):
             last_statistics = self.step(trainer, epoch)
         return last_statistics
 
-    def plot_gpr(self, filename, hparam, hparam_idx):
+    def plot_gpr(self, filename, hparam, hparam_idx, epoch):
         N = 10000
         X_START = self.hparam_ranges[hparam][0]
         X_STOP = self.hparam_ranges[hparam][1]
@@ -120,6 +118,12 @@ class GPUCBAlgo(RLAlgorithm):
         sampled_hparams[:, hparam_idx] = X_bel
         mean_bel, std_bel = self.regressor.predict(sampled_hparams,
                                                    return_std=True)
+        tau_t = 2 * np.log(
+            (1 + epoch) ** (self.n_hparams / 2 + 2) *
+            np.pi**2 / (3 * self.ucb_delta)
+        )  # 2*log(t^(d/2+2)π^2/(3δ))
+        k_t = np.sqrt(self.ucb_nu * tau_t)
+        ucb = mean_bel + k_t * std_bel
 
         plt.clf()
         # plt.ylim((-5, 5))
@@ -129,11 +133,18 @@ class GPUCBAlgo(RLAlgorithm):
         plt.plot(X_bel, mean_bel, label="Mean prediction")
         plt.fill_between(
             X_bel,
-            mean_bel - 1.96 * std_bel,
-            mean_bel + 1.96 * std_bel,
+            mean_bel - ucb,
+            mean_bel + ucb,
             alpha=0.5,
-            label=r"95% confidence interval",
+            label=r"UCB Decision Surface",
         )
+        # plt.fill_between(
+            # X_bel,
+            # mean_bel - 1.96 * std_bel,
+            # mean_bel + 1.96 * std_bel,
+            # alpha=0.5,
+            # label=r"95% confidence interval",
+        # )
         plt.legend()
         plt.xlabel(f"{hparam}")
         plt.ylabel("Perf. Improvement")
