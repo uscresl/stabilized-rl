2a6e9ebf92eab344231f2b4b931f06b417f7e6b9
K.R. Zentner <krzentner@gmail.com>
2023-09-25 00:01:28 -0700

    Start Meta-World xPPO Tianshou experiments

--- exps_tianshou.py
+++ exps_tianshou.py
@@ -34,7 +34,7 @@ if HOST == BRAIN_HOSTNAME:
                 print(f"Running {len(GLOBAL_CONTEXT.running)} jobs")
             # Someone is waiting (maybe us), don't start any more jobs
             GLOBAL_CONTEXT.max_concurrent_jobs = len(GLOBAL_CONTEXT.running) - 1
-    MAX_CONCURRENT_JOBS = 100
+    MAX_CONCURRENT_JOBS = 200
     if GLOBAL_CONTEXT.max_concurrent_jobs > MAX_CONCURRENT_JOBS:
         GLOBAL_CONTEXT.max_concurrent_jobs = MAX_CONCURRENT_JOBS
 
@@ -53,6 +53,60 @@ mujoco_env_names_v3 = [
 EPOCHS = 500  # Basically, this determines the x-axis plot resolution for Tianshou logs
 
 
+MT50_ENV_NAMES = [
+    "assembly",
+    "basketball",
+    "bin-picking",
+    "box-close",
+    "button-press-topdown",
+    "button-press-topdown-wall",
+    "button-press",
+    "button-press-wall",
+    "coffee-button",
+    "coffee-pull",
+    "coffee-push",
+    "dial-turn",
+    "disassemble",
+    "door-close",
+    "door-lock",
+    "door-open",
+    "door-unlock",
+    "hand-insert",
+    "drawer-close",
+    "drawer-open",
+    "faucet-open",
+    "faucet-close",
+    "hammer",
+    "handle-press-side",
+    "handle-press",
+    "handle-pull-side",
+    "handle-pull",
+    "lever-pull",
+    "peg-insert-side",
+    "pick-place-wall",
+    "pick-out-of-hole",
+    "reach",
+    "push-back",
+    "push",
+    "pick-place",
+    "plate-slide",
+    "plate-slide-side",
+    "plate-slide-back",
+    "plate-slide-back-side",
+    "peg-unplug-side",
+    "soccer",
+    "stick-push",
+    "stick-pull",
+    "push-wall",
+    "reach-wall",
+    "shelf-place",
+    "sweep-into",
+    "sweep",
+    "window-open",
+    "window-close",
+]
+
+
 def mujoco_xppo_tianshou(
     seed, env, group, priority=None, cores=2, add_to_path=None, total_steps=None, **kwargs
 ):
@@ -68,6 +122,39 @@ def mujoco_xppo_tianshou(
     return cmd(
         "python",
         "src/mujoco_xppo_tianshou.py",
+        "--seed",
+        seed,
+        "--env",
+        env,
+        "--epoch", EPOCHS,
+        "--step-per-epoch", math.ceil(total_steps / EPOCHS),
+        *[f"--{k.replace('_', '-')}={v}" for (k, v) in kwargs.items()],
+        "--wandb-entity", WANDB_ENTITY,
+        "--wandb-group",
+        group,
+        "--log-dir",
+        Out(f"xppo_tianshou/env={env}_seed={seed}_{kwargs_path}_group={group}/"),
+        warmup_time=3,
+        ram_gb=6,
+        priority=priority,
+        cores=cores,
+    )
+
+def metaworld_xppo_tianshou(
+    seed, env, group, priority=None, cores=2, add_to_path=None, total_steps=None, **kwargs
+):
+    if total_steps is None:
+        total_steps = 20_000_000
+    if priority is None:
+        priority = (50, -seed)
+    if add_to_path is None:
+        add_to_path = [k for k, _ in kwargs.items()][:5]
+    kwargs_path = "_".join(
+        f"{k.replace('_', '-')}={kwargs.get(k)}" for k in add_to_path
+    )
+    return cmd(
+        "python",
+        "src/metaworld_xppo_tianshou.py",
         "--seed",
         seed,
         "--env",
@@ -111,10 +198,19 @@ if HOST == BRAIN_HOSTNAME:
     #                 priority=(60, -seed),
     #                 cores=cores,
     #             )
+    for seed in seeds[:3]:
+        for env in MT50_ENV_NAMES:
+            metaworld_xppo_tianshou(
+                seed=seed,
+                env=env,
+                group="xppo-tianshou-metaworld",
+                priority=(60, -seed),
+            )
+
     for seed in seeds:
         for env_i, env in enumerate(mujoco_env_names_v3):
             for eps_kl_args in [{"eps_kl": 0.2}, {}, {"eps_kl": 1.0}]:
-                for target_coeff in [1, 2, 3, 5, 10]:
+                for target_coeff in [2, 3, 5]:
                     mujoco_xppo_tianshou(
                         seed=seed,
                         env=env,
