1ef5c56690e6a9977be3b252e31b145c371377cc
K.R. Zentner <krzentner@gmail.com>
2023-09-25 22:36:34 -0700

    Cleanup install

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -xe
 
-#python3 -m venv venv
-#source venv/bin/activate
+if [ ! -d venv_install ]; then
+  python3 -m venv venv_install
+fi
+source venv_install/bin/activate
+
 pip install -e .
+pip install git+https://github.com/krzentner/doexp.git@main
 pip install -e ./tianshou
 pip install -e ./metaworld
+pip install scipy
 pip install shimmy
 pip install envpool
 pip install "gym<0.26.0"
