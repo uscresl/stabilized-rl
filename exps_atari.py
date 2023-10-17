559445bd85ea9cc85de034df1220f64a26e4a97c
K.R. Zentner <krzentner@gmail.com>
2023-10-16 18:29:44 -0700

    Actually start _different_ atari experiments

--- exps_atari.py
+++ exps_atari.py
@@ -10,13 +10,15 @@ atari_env_names = [
 ]
 seeds = list(range(10))
 
-GLOBAL_CONTEXT.max_concurrent_jobs = 1
+GLOBAL_CONTEXT.max_concurrent_jobs = 2
 
 for seed in seeds:
     for env_i, env in enumerate(atari_env_names):
-        group = "xppo-atari-real"
+        group = "xppo-atari-real2"
         cmd("python", "src/atari_xppo_tianshou.py", "--log-dir",
             Out(f"xppo_tianshou/env={env}_seed={seed}_group={group}/"),
             "--wandb-group", group,
+            "--env", env,
+            "--seed", seed,
             gpu_ram_gb=2.5, cores=8, ram_gb=20,
             priority=(50, -env_i, -seed))
