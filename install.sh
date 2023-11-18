e966bd7b129220c6456498662cd71b716f3afddb
K.R. Zentner <krzentner@gmail.com>
2023-11-18 11:33:25 -0800

    Add TRPO to plots

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash -xe
 
-if [ ! -d venv_install ]; then
-  python3 -m venv venv_install
+sudo apt-get install
+# If .venv doesn't exist, create it
+if [ ! -d .venv ]; then
+  python3 -m venv .venv
 fi
-source venv_install/bin/activate
+
+# If not in a virtual env, activate .venv
+if [[ "$VIRTUAL_ENV" == "" ]]
+then
+  source .venv/bin/activate
+fi
 
 echo "Using python: $(which python)"
+pip install --upgrade pip
+poetry install
 pip install setuptools
-pip install -e .
-pip install -e ./doexp
-pip install -e ./tianshou
-pip install -e ./metaworld
-pip install scipy
-pip install shimmy
-pip install envpool
-pip install "cython<3"
-pip install "gym==0.22.0"
+#pip install -e .
+#pip install -e ./doexp
+#pip install -e ./tianshou
+#pip install -e ./metaworld
+#pip install scipy
+#pip install shimmy
+#pip install envpool
+#pip install "cython<3"
+#pip install "gym==0.22.0"
 
 pushd trust_region_layers_cpp_deps/nlopt
-cmake && make .
+cmake . && make .
 popd
 pip install -e trust-region-layers/cpp_projection
