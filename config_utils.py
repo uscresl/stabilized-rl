79632f588435cce56b2feaf4aa3032d883fc2e5a
K.R. Zentner <krzentner@gmail.com>
2024-01-04 19:17:01 -0800

    WIP simple_parsing + optuna

--- config_utils.py
+++ config_utils.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field, fields
-from typing import Any, List, Type
+from typing import Any, List, Type, Optional
+import os
 
+import stick
 import optuna
 import simple_parsing
 
@@ -22,12 +24,13 @@ def _get_config_inner(config_type, default=None):
     parser = simple_parsing.ArgumentParser(
         nested_mode=simple_parsing.NestedMode.WITHOUT_ROOT)
     parser.add_arguments(config_type, dest='config_arguments', default=default)
-    parser.add_argument('--cfg-path', default=None, type=str)
+    parser.add_argument('--config', default=None, type=str)
     # print(parser.equivalent_argparse_code())
     args = parser.parse_args()
-    if default is None and args.cfg_path is not None:
+    if default is None and args.config is not None:
         # We haven't loaded defaults from a config, and we're being asked to.
-        cfg_loaded = simple_parsing.helpers.serialization.load(config_type, args.cfg_path)
+        cfg_loaded = simple_parsing.helpers.serialization.load(config_type,
+                                                               args.config)
         return _get_config_inner(config_type, default=cfg_loaded)
     else:
         return args.config_arguments
@@ -39,6 +42,10 @@ def to_yaml(obj) -> str:
 
 def save_yaml(obj, path):
     simple_parsing.helpers.serialization.save_yaml(obj, path)
+
+
+def load_yaml(obj_type, path):
+    return simple_parsing.helpers.serialization.load(obj_type, path)
 
 
 class CustomDistribution:
@@ -87,3 +94,93 @@ def suggest_config(trial: optuna.Trial, config: Type, **kwargs):
             else:
                 sampled[f.name] = trial._suggest(f.name, dist)
     return config(**kwargs, **sampled)
+
+
+@dataclass
+class AlgoConfig(Config):
+    seed: int = 0
+    exp_name: Optional[str] = None
+    log_dir: Optional[str] = None
+
+    '''Used for selecting the best checkpoint and for hparam optimization'''
+    minimization_objective: Optional[str] = None
+
+
+def prepare_training_directory(cfg: AlgoConfig):
+    os.makedirs(cfg.log_dir, exist_ok=True)
+    save_yaml(cfg, os.path.join(cfg.log_dir, 'config.yaml'))
+    # stick will handle seeding for us
+    stick.init_extra(log_dir=cfg.log_dir, run_name=cfg.exp_name,
+                     config=cfg.to_dict())
+
+
+class ExperimentInvocation:
+
+    def __init__(self, train_fn, config_type):
+        self.parser = simple_parsing.ArgumentParser(
+            nested_mode=simple_parsing.NestedMode.WITHOUT_ROOT)
+        self.parser.add_argument('--config', default=None, type=str)
+        self.parser.add_arguments(config_type, dest='cfg')
+
+        def _train():
+            cfg = get_config(config_type)
+            prepare_training_directory(cfg)
+            train_fn(cfg)
+
+        def _sample_config():
+            study = optuna.load_study(storage=self.args.study_storage,
+                                      study_name=self.args.study_name)
+            trial = study.ask()
+            cfg = suggest_config(trial, config_type)
+            save_yaml(cfg, self.args.out_path)
+            base_path = os.path.splitext(self.args.out_path)[0]
+            save_yaml({
+                'trial_number': trial.number,
+                'study_storage': self.args.study_storage,
+                'study_name': self.args.study_name,
+                'config': cfg,
+            }, f"{base_path}-optuna.yaml")
+
+        def _create_study():
+            optuna.create_study(storage=self.args.study_storage,
+                                study_name=self.args.study_name)
+
+        def _report_trial():
+            trial_data = load_yaml(dict, self.args.trial_file)
+            study = optuna.load_study(storage=trial_data['study_storage'],
+                                      study_name=trial_data['study_name'])
+            result_key = self.args.config.minimization_objective
+            # TODO(kr): This function isn't implemented yet (and should it be?)
+            results = stick.load_log_file(self.args.log_file,
+                                          keys=[result_key])
+            study.tell(trial_data["trial_number"], min(results[result_key]))
+
+        self.parser.add_argument('--done-token', type=str, default=None)
+        subp = self.parser.add_subparsers()
+        train_parser = subp.add_parser("train")
+        train_parser.set_defaults(func=_train)
+        create_parser = subp.add_parser("create-study")
+        create_parser.set_defaults(func=_create_study)
+        create_parser.add_argument('--study-storage', type=str)
+        create_parser.add_argument('--study-name', type=str)
+        report_trial = subp.add_parser("report-trial")
+        report_trial.set_defaults(func=_report_trial)
+        report_trial.add_argument('--trial-file', type=str)
+        report_trial.add_argument('--log-file', type=str)
+        sample_parser = subp.add_parser("sample-config")
+        sample_parser.set_defaults(func=_sample_config)
+        sample_parser.add_argument('--study-storage', type=str)
+        sample_parser.add_argument('--study-name', type=str)
+        sample_parser.add_argument('--out-path', type=str)
+        self.args = self.parser.parse_args()
+        if self.args.config:
+            loaded_config = load_yaml(config_type, self.args.config)
+            self.parser.set_defaults(cfg=loaded_config)
+            self.args = self.parser.parse_args()
+
+    def run(self):
+        self.args = self.parser.parse_args()
+        self.args.func()
+        if self.args.done_token:
+            with open(self.args.done_token, 'w') as f:
+                f.write('done\n')
